338375aa5c046c11e4621d2d929d0006bb1925e3
Christoph <christoph@ag-prop.com>
2021-09-23 19:04:29 +0200

    Feat(Pods): Use pods for routes

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'flimmerkasten-client',
+    podModulePrefix: 'flimmerkasten-client/routes',
     environment,
     rootURL: '/',
     locationType: 'auto',
