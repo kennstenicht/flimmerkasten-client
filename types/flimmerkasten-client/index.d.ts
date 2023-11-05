05d64b1e4a863ab15e9f579a01d4fa8304e5250d
Christoph Wiedenmann <christoph@ag-prop.com>
2023-11-05 22:48:34 +0100

    Fix(Types): Clean unused types

--- types/flimmerkasten-client/index.d.ts
+++ types/flimmerkasten-client/index.d.ts
@@ -1,7 +1,3 @@
 import 'ember-source/types';
 import '@glint/environment-ember-loose';
 import '@glint/environment-ember-template-imports';
-
-declare global {}
-
-export {};
--- types/global.d.ts
+++ types/global.d.ts
@@ -1,10 +1,3 @@
-// Types for compiled templates
-declare module 'flimmerkasten-client/templates/*' {
-  import { TemplateFactory } from 'htmlbars-inline-precompile';
-  const tmpl: TemplateFactory;
-  export default tmpl;
-}
-
 declare module '*.css' {
   const styles: { [className: string]: string };
   export default styles;
